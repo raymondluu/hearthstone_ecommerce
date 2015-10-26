8fa41851e3fac5ac27c5e6eb24b0ce961a3c59b1
raymondluu <Ray@Raymonds-MBP.hsd1.wa.comcast.net>
2015-10-26 16:17:49 -0700

    added nav.php partial file and included the links necessary for nav bar

--- application/views/index.php
+++ application/views/index.php
@@ -6,25 +6,10 @@
   <?php $this->load->view("/partials/head.php") ?>
 </head>
 <body>
-  <div class="container-fluid">
-  <h1>Welcome to Hearthstone Ecommerce site!</h1>
-<?php
-  // The unirest library is in the views folder already!
-  $this->load->view('/unirest-php/src/Unirest.php');
+	<?php $this->load->view("/partials/nav.php") ?>
+	<div class="container-fluid">
+		<h1>Welcome to Hearthstone Ecommerce site!</h1>
 
-  $response = Unirest\Request::get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/EX1_572",
-    array
-    (
-    //THIS KEY IS PRIVATE USE UR OWNNNN!!!!
-    "X-Mashape-Key" => "h1qylTCmitmshxurlWa9zZuVvvb5p10IB58jsnIYzfQeooEdLv",
-    "Accept" => "json"
-    )
-  );
-  var_dump($response);
-?>
-
-<!-- the php echo'd here is how you get the img, change "img" to any atrribute in the object! -->
-<img src="<?= $response->body[0]->img; ?>" >
-  </div>
+	</div>
 </body>
 </html>
---
+++ application/views/partials/nav.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<p class="navbar-brand">Hearthstone Ecommerce</p>
+		</div>
+		<div class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="#">Orders</a></li>
+				<li><a href="#">Products</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+              <li><a href="/signin">Shopping Cart (5)</a></li>
+              <li><a href="#">Log off</a></li>
+            </ul>
+		</div>
+	</div>
+</nav>
