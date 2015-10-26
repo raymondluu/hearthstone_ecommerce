69df14dbdb1b2a42ad64b23183be2bb4952d8ce2
raymondluu <Ray@Raymonds-MBP.hsd1.wa.comcast.net>
2015-10-26 13:25:51 -0700

    second commit with code igniter files

---
+++ application/views/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hearthstone Ecommerce Site</title>
+	<?php $this->load->view("/partials/head.php") ?>
+</head>
+<body>
+	<div class="container-fluid">
+		<h1>Welcome to Hearthstone Ecommerce site!</h1>
+	</div>
+</body>
+</html>
