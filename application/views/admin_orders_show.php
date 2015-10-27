86e5194f3b8e0e4b89600ae75336ca8acba292fb
smehlhope <stephanie.mehlhope@gmail.com>
2015-10-26 22:37:02 -0700

    initial commit of html/css for admin orders table pages and the client side carts view

---
+++ application/views/admin_orders_show.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-6">
+    <title>Hearthstone Ecommerce Site</title>
+    <?php $this->load->view("/partials/head.php"); ?>
+    <link rel="stylesheet" type="text/css" href="/assets/bootstrap-3.3.5-dist/css/bootstrap.css">
+</head>
+<body>
+
+    <div class="col-lg-4">
+        <table class="table table-striped table-bordered table-condensed">
+            <tr>
+                <th>Order ID:</th>
+                <td>1</td>
+            </tr>
+            <tr>
+                <th>Customer Shipping Info:</th>
+                <td>2</td>
+            </tr>
+            <tr>
+                <th>Name:</th>
+                <td>3</td>
+            </tr>
+            <tr>
+                <th>Address:</th>
+                <td>4</td>
+            </tr>
+            <tr>
+                <th>City</th>
+                <td>5</td>
+            </tr>
+            <tr>
+                <th>State:</th>
+                <td>6</td>
+            </tr>
+            <tr>
+                <th>Zip:</th>
+                <td>7</td>
+            </tr>
+            <tr>
+                <th>Customer Billing Info:</th>
+                <td>8</td>
+            </tr>
+            <tr>
+                <th>Name:</th>
+                <td>9</td>
+            </tr>
+            <tr>
+                <th>Address:</th>
+                <td>10</td>
+            </tr>
+            <tr>
+                <th>City</th>
+                <td>11</td>
+            </tr>
+            <tr>
+                <th>State:</th>
+                <td>12</td>
+            </tr>
+            <tr>
+                <th>Zip:</th>
+                <td>13</td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="col-lg-8">
+            <table class="table table-striped table-bordered table-condensed">
+                <thead>
+                    <th class="col-lg-2">Picture</th>
+                    <th class="col-lg-1">ID</th>
+                    <th class="col-lg-2">Name</th>
+                    <th class="col-lg-1">Inventory Count</th>
+                    <th class="col-lg-1">Quantity Sold</th>
+                    <th class="col-lg-2">Action</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>IMAGE HERE</td>
+                        <td>1</td>
+                        <td>Ysera Gold</td>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>
+                            <a href="products/edit">Edit</a>
+                            <a href="products/delete">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>IMAGE HERE</td>
+                        <td>1</td>
+                        <td>Ysera Normal</td>
+                        <td>15</td>
+                        <td>25</td>
+                        <td>
+                            <a href="products/edit">Edit</a>
+                            <a href="products/delete">Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+    </div>
+
+    <div class="col-lg-8">
+        <h3>ECHO SHIPPING STATUS</h3>
+        <div class="col-lg-4">
+            <p>Subtotal: $</p>
+            <p>Shipping: $</p>
+            <p>Total: $</p>
+
+        </div>
+    </div>    
+
+</body>
+<html>
