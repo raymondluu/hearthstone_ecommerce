fff4aacb5465f819bc553796efe9b9fb8914ed4d
raymondluu <Ray@Raymonds-MacBook-Pro.local>
2015-10-26 21:35:33 -0700

    added pagination at the bottom of page

--- application/views/index.php
+++ application/views/index.php
@@ -7,25 +7,25 @@
 </head>
 <body>
   <div class="container-fluid">
-  <h1>Welcome to Hearthstone Ecommerce site!</h1>
+  <!-- <h1>Welcome to Hearthstone Ecommerce site!</h1> -->
 <?php
   // The unirest library is in the views folder already!
-  $this->load->view('/unirest/src/Unirest.php');
+  //$this->load->view('/unirest/src/Unirest.php');
 
-  $response = Unirest\Request::get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/EX1_572",
-    array
-    (
+  //$response = Unirest\Request::get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/EX1_572",
+    //array
+    //(
     //THIS KEY IS PRIVATE USE UR OWNNNN!!!!
-    "X-Mashape-Key" => "h1qylTCmitmshxurlWa9zZuVvvb5p10IB58jsnIYzfQeooEdLv",
-    "Accept" => "json"
-    )
-  );
-  var_dump($response);
+    //"X-Mashape-Key" => "h1qylTCmitmshxurlWa9zZuVvvb5p10IB58jsnIYzfQeooEdLv",
+    //"Accept" => "json"
+    //)
+  //);
+  //var_dump($response);
 ?>
 
 <!-- the php echo'd here is how you get the img, change "img" to any atrribute in the object! -->
-<img src="<?= $response->body[0]->img; ?>" >
-<a href="/product_description">Link to Product Descriptions</a>
+<!-- <img src="<?= $response->body[0]->img; ?>" > -->
+<!-- <a href="/product_description">Link to Product Descriptions</a> -->
   </div>
 	<?php $this->load->view("/partials/nav.php") ?>
 	<div class="container-fluid">
@@ -46,6 +46,25 @@
 			</div>
 			<div class="col-md-10">
 				<h1>Tshirts (page 2)</h1>
+				<nav>
+  <ul class="pagination">
+    <li>
+      <a href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li><a href="#">1</a></li>
+    <li><a href="#">2</a></li>
+    <li><a href="#">3</a></li>
+    <li><a href="#">4</a></li>
+    <li><a href="#">5</a></li>
+    <li>
+      <a href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
 			</div>
 		</div>
 	</div>
