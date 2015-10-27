606f3b515c1cea4eaeeeb9f763007f1f07bc8e8c
raymondluu <Ray@Raymonds-MacBook-Pro.local>
2015-10-26 21:05:23 -0700

    added style.css and updated index.php page

--- application/views/index.php
+++ application/views/index.php
@@ -8,8 +8,25 @@
 <body>
 	<?php $this->load->view("/partials/nav.php") ?>
 	<div class="container-fluid">
-		<h1>Welcome to Hearthstone Ecommerce site!</h1>
-
+		<div class="row">
+			<div class="col-md-2">
+				<form>
+					<input type="text" name="search" placeholder="product name">
+					<input type="submit" name="action" value="search">
+				</form>
+				<p>Categories</p>
+				<ul>
+					<li>Tshirts (25)</li>
+					<li>Shoes (35)</li>
+					<li>Cups (5)</li>
+					<li>Fruits (105)</li>
+					<li>Show all</li>
+				</ul>
+			</div>
+			<div class="col-md-10">
+				<h1>Tshirts (page 2)</h1>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
