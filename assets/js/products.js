e07fd3868c9b02737c78bd161061eb258f6b0761
raymondluu <Ray@Raymonds-MBP.hsd1.wa.comcast.net>
2015-10-27 16:01:18 -0700

    categories feature added

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,14 +1,67 @@
 $(document).ready(function(){
-	$("#showall").on("click", function(){
-		$.get("/products/get_cards_json", function(data){
-			var card = data;
+	$("a").on("click", function(){
+		var id = $(this).attr("id");
+		var url = "";
+		if(id == "catheros")
+		{
+			title_str = "Heros";
+			url = "/products/get_cards_by_type_json/hero";
+		}
+		else if(id == "catenchants")
+		{
+			title_str = "Enchantments";
+			url = "/products/get_cards_by_type_json/enchantment";
+		}
+		else if(id == "catspells")
+		{
+			title_str = "Spells";
+			url = "/products/get_cards_by_type_json/spell";
+		}
+		else if(id == "catweapons")
+		{
+			title_str = "Weapons";
+			url = "/products/get_cards_by_type_json/weapon";
+		}
+		else if(id == "catminions")
+		{
+			title_str = "Minions";
+			url = "/products/get_cards_by_type_json/minion";
+		}
+		else
+		{
+			title_str = "Show all";
+			url = "/products/get_cards_json";
+		}
+
+		$.get(url, function(data){
 			var img_str = "";
-			for(var i = 0; i < card.length; i++)
+			console.log(data);
+			for(var i = 0; i < data.cards.length; i++)
 			{
 				// console.log('card[i].img');
-				img_str += " <img class='products' src=' "+ card[i].img +" '>";
+				img_str += " <img class='products' src=' "+ data.cards[i].img +" '>";
 			}
+			$('#title').html(title_str);
 			$('#pictureFrame').html(img_str);
 		}, "json");
+
+
 	});
+
+
+	$.get("/products/get_cards_by_type_json/hero", function(data){
+		$('#catheros').append("Heros (" + data.count + ")");
+	}, "json");
+	$.get("/products/get_cards_by_type_json/enchantment", function(data){
+		$('#catenchants').append("Enchantments (" + data.count + ")");
+	}, "json");
+	$.get("/products/get_cards_by_type_json/spell", function(data){
+		$('#catspells').append("Spells (" + data.count + ")");
+	}, "json");
+	$.get("/products/get_cards_by_type_json/weapon", function(data){
+		$('#catweapons').append("Weapons (" + data.count + ")");
+	}, "json");
+	$.get("/products/get_cards_by_type_json/minion", function(data){
+		$('#catminions').append("Minions (" + data.count + ")");
+	}, "json");
 });
