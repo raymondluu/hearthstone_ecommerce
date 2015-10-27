7ee9c8b744b7d8263b7ebe49eb907a150e19e27c
timlichen <tim.li.chen@gmail.com>
2015-10-27 11:36:08 -0700

    database stuff

---
+++ application/db/schema_gen_partial.sql
@@ -0,0 +1,7 @@
+DROP SCHEMA IF EXISTS `hearthstone` ;
+
+-- -----------------------------------------------------
+-- Schema hearthstone
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `hearthstone` DEFAULT CHARACTER SET latin1 ;
+USE `hearthstone` ;
