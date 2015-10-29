e711251a95ac22f08107c9aa7285c523a08fdba6
raymondluu <Ray@Raymonds-MBP.hsd1.wa.comcast.net>
2015-10-29 15:55:50 -0700

    navbar changes

--- application/views/partials/nav.php
+++ application/views/partials/nav.php
@@ -1,13 +1,15 @@
 <nav class="navbar navbar-default style-header">
 	<div class="container-fluid">
-    <div class="navbar-brand">
-       <img class="mini_logo top" src="/assets/Card_Back_Esports.gif"></img>
-       <img class="mini_logo middle" src="/assets/Card_Back_Esports.gif"></img>
-       <img class="mini_logo bottom" src="/assets/Card_Back_Esports.gif"></img>
-    </div>
-		<div class="navbar-header padding_left">
-			 <p class="nav_header_text">Hearthstone Ecommerce</p>
-		</div>
+    <a href="/">
+      <div class="navbar-brand">
+         <img class="mini_logo top" src="/assets/Card_Back_Esports.gif"></img>
+         <img class="mini_logo middle" src="/assets/Card_Back_Esports.gif"></img>
+         <img class="mini_logo bottom" src="/assets/Card_Back_Esports.gif"></img>
+      </div>
+  		<div class="navbar-header padding_left">
+  			 <p class="nav_header_text">Hearthstone Ecommerce</p>
+  		</div>
+    </a>
 		<div class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-right">
               <ol id="cart-ol"><a href="/carts">Shopping Cart (
