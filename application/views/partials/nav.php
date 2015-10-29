3adf8fe72d6923fc443367977b897843ce67e6b5
raymondluu <Ray@Raymonds-MBP.hsd1.wa.comcast.net>
2015-10-29 15:31:37 -0700

    edit nav bar and styling it

--- application/views/partials/nav.php
+++ application/views/partials/nav.php
@@ -1,7 +1,12 @@
 <nav class="navbar navbar-default">
 	<div class="container-fluid">
-		<div class="navbar-header">
-			<h1 class="navbar-h1">Hearthstone Ecommerce</h1>
+    <div class="navbar-brand">
+       <img class="mini_logo top" src="http://wow.zamimg.com/images/hearthstone/backs/animated/Card_Back_Esports.gif"></img>
+       <img class="mini_logo middle" src="http://wow.zamimg.com/images/hearthstone/backs/animated/Card_Back_Esports.gif"></img>
+       <img class="mini_logo bottom" src="http://wow.zamimg.com/images/hearthstone/backs/animated/Card_Back_Esports.gif"></img>
+    </div>
+		<div class="navbar-header padding_left">
+			 <p class="nav_header_text">Hearthstone Ecommerce</p>
 		</div>
 		<div class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-right">
