b78a22569ba0f664b6c0b7a22741dff920eb42fb
timlichen <tim.li.chen@gmail.com>
2015-10-30 09:30:51 -0700

    fixing up the master

--- application/controllers/orders.php
+++ application/controllers/orders.php
@@ -3,43 +3,7 @@
 class Orders extends CI_Controller {
 
 	public function admin_orders() {
-<<<<<<< HEAD
-	    //NEED TO LOAD ALL ORDERS
-	    $orders['orders'] = $this->order->get_all_orders();
-	    // var_dump($orders);
-	    $this->load->view('admin_orders', $orders);
-  	}
 
-  	public function show_order($order_id) {
-  		$order = $this->order->get_one_order_for_show($order_id);
-  		$this->load->view('admin_orders_show', array("order_info" => $order));
-  	}
-
-  	public function set_status_shipped($order_id){
-  		$this->order->set_status_shipped($order_id);
-  		$order = $this->order->get_one_order_for_show($order_id);
-  		$this->load->view('admin_orders_show', array("order_info" => $order));
-  	}
-
-  	public function set_status_in_process($order_id){
-  		$this->order->set_status_in_process($order_id);
-  		$order = $this->order->get_one_order_for_show($order_id);
-  		$this->load->view('admin_orders_show', array("order_info" => $order));
-  	}
-
-  	public function set_status_cancelled($order_id){
-  		$this->order->set_status_cancelled($order_id);
-  		$order = $this->order->get_one_order_for_show($order_id);
-  		$this->load->view('admin_orders_show', array("order_info" => $order));
-  	}
-    public function submit_billing()
-    {
-      var_dump($this->input->post());
-      var_dump($this->session->userdata['session_id']);
-      die();
-    }
-
-=======
     if($this->session->userdata['user_data'] != 'admin'){
       $this->session->set_flashdata('admin', 'You must be an logged in admin to access that page.');
       redirect("/");
@@ -64,8 +28,6 @@ class Orders extends CI_Controller {
     }
   }
   public function set_status_shipped($session_id){
->>>>>>> development
-
     if($this->session->userdata['user_data'] != 'admin'){
       $this->session->set_flashdata('admin', 'You must be an logged in admin to access that page.');
       redirect("/");
